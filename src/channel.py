0f9dbca047b95ea7c810ffec658f4582676df43c
munquzbek <munchik31@gmail.com>
2023-09-02 05:27:11 -0400

    Homework#2

--- src/channel.py
+++ src/channel.py
@@ -7,16 +7,34 @@ youtube = build('youtube', 'v3', developerKey=api_key)
 
 
 class Channel:
-    """Класс для ютуб-канала"""
-
     def __init__(self, channel_id: str) -> None:
-        """Экземпляр инициализируется id канала. Дальше все данные будут подтягиваться по API."""
         self.channel_id = channel_id
+        data = youtube.channels().list(id=channel_id, part='snippet,statistics').execute()
+        for d in data["items"]:
+            self.title = d['snippet']['title']
+            self.description = d['snippet']['description']
+            self.url = d['snippet']['thumbnails']['default']['url']
+            self.subscriber_count = d['statistics']['subscriberCount']
+            self.video_count = d['statistics']['videoCount']
+            self.view_count = d['statistics']['viewCount']
 
-    def print_info(self) -> None:
-        request_to_data = youtube.channels().list(
-            part="snippet,statistics",
-            id=self.channel_id
-        )
-        response = request_to_data.execute()
-        print(json.dumps(response, indent=2, ensure_ascii=False))
+    @classmethod
+    def get_service(cls):
+        youtube = build('youtube', 'v3', developerKey=api_key)
+        return youtube
+
+    def to_json(self, where_to_save):
+        dictionary = {
+            'title': self.title,
+            'channel id': self.channel_id,
+            'description': self.description,
+            'url': self.url,
+            'subsciber count': self.subscriber_count,
+            'video count': self.video_count,
+            'view count': self.view_count
+        }
+
+        with open(where_to_save, "w") as outfile:
+            json.dump(dictionary, outfile, indent=4)
+
+
