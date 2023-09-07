b9a6cf0c87bcbf66e0cf2a17e16698df02d95b8b
munquzbek <munchik31@gmail.com>
2023-09-07 02:13:40 -0400

    Homework#2.1

--- src/channel.py
+++ src/channel.py
@@ -3,20 +3,18 @@ import json
 from googleapiclient.discovery import build
 
 api_key: str = os.getenv('YT_API_KEY')
-youtube = build('youtube', 'v3', developerKey=api_key)
 
 
 class Channel:
     def __init__(self, channel_id: str) -> None:
         self.channel_id = channel_id
-        data = youtube.channels().list(id=channel_id, part='snippet,statistics').execute()
-        for d in data["items"]:
-            self.title = d['snippet']['title']
-            self.description = d['snippet']['description']
-            self.url = d['snippet']['thumbnails']['default']['url']
-            self.subscriber_count = d['statistics']['subscriberCount']
-            self.video_count = d['statistics']['videoCount']
-            self.view_count = d['statistics']['viewCount']
+        data = self.get_service().channels().list(id=channel_id, part='snippet,statistics').execute()
+        self.title = data['items'][0]['snippet']['title']
+        self.description = data['items'][0]['snippet']['description']
+        self.url = data['items'][0]['snippet']['thumbnails']['default']['url']
+        self.subscriber_count = data['items'][0]['statistics']['subscriberCount']
+        self.video_count = data['items'][0]['statistics']['videoCount']
+        self.view_count = data['items'][0]['statistics']['viewCount']
 
     @classmethod
     def get_service(cls):
@@ -35,6 +33,6 @@ class Channel:
         }
 
         with open(where_to_save, "w") as outfile:
-            json.dump(dictionary, outfile, indent=4)
+            json.dump(dictionary, outfile, indent=4, ensure_ascii=False)
 
 
